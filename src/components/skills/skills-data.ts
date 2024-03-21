78f3f4a2048486db09c932055f24f4c59e864cc7
Bruno Alves <nito.dev@gmail.com>
2024-03-21 16:00:17 -0300

    feat: add skills section

---
+++ src/components/skills/skills-data.ts
@@ -0,0 +1,62 @@
+export const skills = [
+  {
+    title: 'React',
+    icon: '/icons/react-js.svg',
+  },
+  {
+    title: 'NextJs',
+    icon: '/icons/next-js.svg',
+  },
+  {
+    title: 'Tailwindcss',
+    icon: '/icons/tailwindcss.svg',
+  },
+  {
+    title: 'Typescript',
+    icon: '/icons/typescript.svg',
+  },
+  {
+    title: 'NodeJs',
+    icon: '/icons/node-js.svg',
+  },
+  {
+    title: 'Fastify',
+    icon: '/icons/fastify.svg',
+  },
+  {
+    title: 'NestJs',
+    icon: '/icons/nest-js.svg',
+  },
+  {
+    title: 'SQL',
+    icon: '/icons/sql.svg',
+  },
+  {
+    title: 'Prisma',
+    icon: '/icons/prisma.svg',
+  },
+  {
+    title: 'React Native',
+    icon: '/icons/react-native.svg',
+  },
+  {
+    title: 'Expo',
+    icon: '/icons/expo.svg',
+  },
+  {
+    title: 'Figma',
+    icon: '/icons/figma.svg',
+  },
+  {
+    title: 'Graphql',
+    icon: '/icons/graphql.svg',
+  },
+  {
+    title: 'Docker',
+    icon: '/icons/docker.svg',
+  },
+  {
+    title: 'Git',
+    icon: '/icons/git.svg',
+  },
+]
