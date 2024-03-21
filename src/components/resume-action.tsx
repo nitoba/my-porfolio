4286bc336aa7095be680e6ddf82285fda1cc83dd
Bruno Alves <nito.dev@gmail.com>
2024-03-21 14:34:34 -0300

    feat: add social card links

--- src/components/resume-action.tsx
+++ src/components/resume-action.tsx
@@ -5,7 +5,12 @@ import type { ComponentProps } from 'react'
 export function ResumeActionButton(props: ComponentProps<typeof Button>) {
   return (
     <Button asChild {...props}>
-      <a href="/download">
+      <a
+        href="https://github.com/nitoba/resume/files/14704923/CV.pdf"
+        download
+        target="_blank"
+        rel="noreferrer"
+      >
         Resume
         <DownloadIcon className="ml-2 size-5" />
       </a>
