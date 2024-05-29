50eaaf5255ed46f689e6e4c365c1bafddfc431f2
Bruno Alves <nito.ba.dev@gmail.com>
2024-05-29 13:55:07 -0300

    feat: adjust layout from lang toggle

--- src/components/lang-toggle.tsx
+++ src/components/lang-toggle.tsx
@@ -13,15 +13,24 @@ export function LangToggle() {
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="sm" className="h-9 min-w-16">
-          {languages[currentLang]}
+        <Button variant="outline" size="sm" className="h-9">
+          {currentLang.toUpperCase()}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        {Object.entries(languages).map(([key, value]) => (
-          <DropdownMenuItem key={key} asChild>
-            <a href={`/${key === defaultLang ? '' : key}`}>{value}</a>
+      <DropdownMenuContent
+        align="end"
+        className="flex min-w-[3.5rem] flex-col items-center justify-center text-center"
+      >
+        {Object.keys(languages).map((key) => (
+          <DropdownMenuItem
+            key={key}
+            asChild
+            className="flex w-full items-center justify-center"
+          >
+            <a href={`/${key === defaultLang ? '' : key}`}>
+              {key.toUpperCase()}
+            </a>
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
