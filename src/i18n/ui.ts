fa21aabef0479ebeb141b11b598f64ad8e128af4
Bruno Alves <nito.ba.dev@gmail.com>
2024-05-30 12:46:25 -0300

    feat: translate skills section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,21 +11,23 @@ export const ui = {
     'header.about': 'About Me',
     'header.projects': 'Projects',
     'header.contact': 'Contact Me',
-
     'hero.welcome': 'Hello I’am',
     'hero.dev': 'Developer',
     'hero.description':
       'My goal is to acquire new skills that will improve my performance and productivity, so that I can grow as a person and as a professional, adding more value to the team project. I have knowledge mainly in the construction of products focused on the areas of eco-commerce, landing-pages, Web three, crowdfunding. Being challenged and keeping myself in constant learning are my motivators.',
+    'skills.my': 'My',
+    'skills.skills': 'Skills',
   },
   pt: {
     'header.skills': 'Habilidades',
     'header.about': 'Sobre',
     'header.projects': 'Projetos',
     'header.contact': 'Contato',
-
     'hero.welcome': 'Olá, eu sou',
     'hero.dev': 'Desenvolvedor',
     'hero.description':
       'Meu objetivo é adquirir novas competências que melhorem meu desempenho e produtividade, para que eu possa crescer como pessoa e como profissional, agregando mais valor ao projeto da equipe. Tenho conhecimento principalmente na construção de produtos voltados para as áreas de eco-commerce, landing-pages, Web three, crowdfunding. Ser desafiado e me manter em constante aprendizado são meus motivadores.',
+    'skills.my': 'Minhas',
+    'skills.skills': 'Habilidades',
   },
 } as const
